18daa445556f692a4f255615cdb9b35e26e2fd9f
dongxubin87 <dongxubin87@gmail.com>
2025-02-20 09:21:12 +0200

    second commit

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,7 +8,7 @@ import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("dong@gmail.com");
-  const [password, setPassword] = useState("dong8808");
+  const [password, setPassword] = useState("11111111");
   const { login, isLoading } = useLogin();
   function handleSubmit(e) {
     e.preventDefault();
